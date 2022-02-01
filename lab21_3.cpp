36ec3c68c885047c86a6b2bacb4405f4ba86e167
bukbunny <khomsan_su@cmu.ac.th>
2022-02-01 16:54:12 +0700

    add &

--- lab21_3.cpp
+++ lab21_3.cpp
@@ -24,7 +24,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
